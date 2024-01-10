9aaa6c61346252cab2036ba99a816ffe3b099bb6
zhanghui <iron.zhang@foxmail.com>
2024-01-10 18:50:52 +0800

    feat: update supermodel

--- superutil/supermodel/model.go
+++ superutil/supermodel/model.go
@@ -2,8 +2,8 @@ package supermodel
 
 // ServiceModel 服务模型
 type ServiceModel struct {
-	Domain   string             // 域名
-	Clusters map[string]Cluster // 集群节点表
+	Domain   string    // 域名
+	Clusters []Cluster // 集群节点列表
 }
 
 // RouteModel 路由模型
