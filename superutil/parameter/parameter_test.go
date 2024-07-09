f45591137c63c6183a4e8873388812bc3d0492e0
zhanghui <iron.zhang@foxmail.com>
2024-07-09 14:57:02 +0800

    test(superutil/parameter): add unit test

--- superutil/parameter/parameter_test.go
+++ superutil/parameter/parameter_test.go
@@ -1,7 +1,15 @@
 package parameter
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ironzhang/superlib/fileutil"
+)
 
 func TestParameter(t *testing.T) {
 	t.Logf("%+v", Param)
+}
+
+func TestWriteParameter(t *testing.T) {
+	fileutil.WriteTOML("supername.conf", Param)
 }
